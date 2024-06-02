1a3eadf7ec90f6c92d8740c1c7962e4abf45d8f0
cathella <ncathie214@gmail.com>
2024-06-02 10:47:32 +0300

    Improved code for nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,22 @@
-import * as React from "react";
+const MenuItem = ({ children }) => {
+  return <a href="#" className="self-stretch my-auto">{children}</a>;
+};
 
 function Nav() {
   return (
-    <div className="flex gap-5 justify-between px-20 py-8 text-lg leading-7 text-black max-md:flex-wrap max-md:px-5">
-      <div className="flex gap-5 justify-between items-center font-medium max-md:flex-wrap">
+    <header className="flex gap-5 justify-between px-20 py-8 text-lg leading-7 text-black max-md:flex-wrap max-md:px-5">
+      <nav className="flex gap-5 justify-between items-center font-medium max-md:flex-wrap">
         <div className="self-stretch text-2xl font-bold">NC</div>
-        <div className="self-stretch my-auto">Home</div>
-        <div className="self-stretch my-auto">Projects</div>
-        <div className="self-stretch my-auto">Services</div>
-        <div className="self-stretch my-auto">About us</div>
-      </div>
+        <MenuItem>Home</MenuItem>
+        <MenuItem>Projects</MenuItem>
+        <MenuItem>Services</MenuItem>
+        <MenuItem>About us</MenuItem>
+      </nav>
       <div className="flex gap-2 self-start font-semibold capitalize">
-        <div className="underline">Start project</div>
-        <img
-          loading="lazy"
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/81969eff1e3b42014519e3e9d8705a7cfddf4fd4cf12baef9a7a6200a091f5aa?"
-          className="shrink-0 my-auto w-6 aspect-square"
-        />
+        <a href="#" className="underline" tabindex="0">Start project</a>
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/81969eff1e3b42014519e3e9d8705a7cfddf4fd4cf12baef9a7a6200a091f5aa?apiKey=27ec22b9382040ef8580a5e340d3a921&" className="shrink-0 my-auto w-6 aspect-square" alt="Project Start Icon" tabindex="0" />
       </div>
-    </div>
+    </header>
   );
 }
 
